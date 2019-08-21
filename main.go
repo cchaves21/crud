fd1c07171114fda3c2993b836c8e3d410b2d9b56
Carlos Chaves <kchaves001@gmail.com>
2019-08-21 12:14:21 -0300

    include insert func

--- main.go
+++ main.go
@@ -16,23 +16,38 @@ const (
 )
 
 func main() {
- psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+ 
+  insert(31, "kchaves@gmail.com", "Carlos", "chaves")
+    
+}
+
+func dbConn() (db *sql.DB) {
+  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
     "password=%s dbname=%s sslmode=disable",
     host, port, user, password, dbname)
   db, err := sql.Open("postgres", psqlInfo)
   if err != nil {
     panic(err)
   }
-  defer db.Close()
+  return db
+}
 
+func insert(age int, email string, first_name string, last_name string) {
+  db := dbConn()
   sqlStatement := `
-INSERT INTO users (age, email, first_name, last_name)
-VALUES ($1, $2, $3, $4)
-RETURNING id`
+  INSERT INTO users (age, email, first_name, last_name)
+  VALUES ($1, $2, $3, $4)
+  RETURNING id`
   id := 0
-  err = db.QueryRow(sqlStatement, 30, "jon@calhoun.io", "Jonathan", "Calhoun").Scan(&id)
-  if err != nil {
-    panic(err)
-  }
+  db.QueryRow(sqlStatement, age, email, first_name, last_name).Scan(&id)
+  //if err != nil {
+    //panic(err)
+  //}
+  defer db.Close()
   fmt.Println("New record ID is:", id)
+}
+
+func update(id int) {
+  db := dbConn()
+
 }
